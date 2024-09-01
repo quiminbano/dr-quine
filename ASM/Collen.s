f55e9f52025830751c9e026890903e6bc70b9682
Carlos Orellana <corellana1988@gmail.com>
2024-09-02 00:05:50 +0300

    asm Collen done

--- ASM/Collen.s
+++ ASM/Collen.s
@@ -1,16 +1,33 @@
 ; This is a comment before program
 section .data
-    string db "; This is a comment before program%1$csection .data%1$c    string db %2$c%3$s$2%c%1$c%1%csection .text%1%c    global start%1$c    extern printf%1$c%1$cstart:%1$c    lea rdi, QWORD[string]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, rdi%1$c    call printf wrt ..plt%1$c    xor rax, rax%1$c    ret%1$c"
+    string db "; This is a comment before program%1$csection .data%1$c    string db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c%1$c_start:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, QWORD string%1$c    call print_me%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c%1$cprint_me:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, rdi%1$c    call printf wrt ..plt%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c", 0
 
 section .text
-    global start
+    global _start
     extern printf
 
-start:
-    lea rdi, QWORD[string]
+_start:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    mov rdi, string
+    call print_me
+    add rsp, 16
+    mov rsp, rbp
+    pop rbp
+    xor rax, rax
+    ret
+
+print_me:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
     mov rsi, 10
     mov rdx, 34
     mov rcx, rdi
     call printf wrt ..plt
+    add rsp, 16
+    mov rsp, rbp
+    pop rbp
     xor rax, rax
     ret
