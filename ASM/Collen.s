485345a24c1ac925296d5a288ea5e48a4527aa0f
Carlos Orellana <corellana1988@gmail.com>
2024-09-03 16:46:52 +0300

    fixed asm

--- ASM/Collen.s
+++ ASM/Collen.s
@@ -1,6 +1,6 @@
 ; This is a comment before program
 section .data
-    string db "; This is a comment before program%1$csection .data%1$c    string db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c%1$c_start:%1$c; This is a comment inside the program%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, QWORD string%1$c    call print_me%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c%1$cprint_me:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, rdi%1$c    call printf wrt ..plt%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c", 0
+    string db "; This is a comment before program%1$csection .data%1$c    string db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c%1$c_start:%1$c; This is a comment inside the program%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, QWORD string%1$c    call print_me%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    mov rax, 60%1$c    mov rdi, 0%1$c    syscall%1$c%1$cprint_me:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, rdi%1$c    call printf wrt ..plt%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    xor rax, rax%1$c    ret%1$c%1$csection .note.GNU-stack%1$c", 0
 
 section .text
     global _start
@@ -34,3 +34,5 @@ print_me:
     pop rbp
     xor rax, rax
     ret
+
+section .note.GNU-stack
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -16,7 +16,7 @@
 %endmacro
 %macro FT_MAIN 0
 section .data
-    string db ";This is a comment in the code%1$c%3$cmacro PRINT_STRING 1%1$c    mov rdi, %3$c1%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, 25%1$c	mov r8, %3$c1%1$c    call printf wrt ..plt%1$c%3$cendmacro%1$c%3$cmacro OPEN_FILE 1%1$c    mov rax, 2%1$c    mov rdi, %3$c1%1$c    mov rsi, 2%1$c    mov rdx, 420%1$c    syscall%1$c%3$cendmacro%1$c%3$cmacro FT_MAIN 0%1$csection .data%1$c    string db %2$c%4$c%2$c%1$c    file_name db %2$cGrace_kid.s%2$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c_start:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    OPEN_FILE file_name%1$c    cmp rax, 0%1$c    jl return_error%1$c    mov rdi, rax%1$c    push rdi%1$c    mov rax, 33%1$c    mov rsi, 1%1$c    syscall%1$c    cmp rax, 0%1$c    jl closefile_and_error%1$c    PRINT_STRING string%1$c    pop rdi%1$c    mov rax, 3%1$c    syscall%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    mov rax, 60%1$c    mov rdi, 0%1$c    syscall%1$cclosefile_and_error:%1$c    pop rdi%1$c    mov rax, 3%1$c    syscall%1$creturn_error:%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    mov rax, 60%1$c    mov rdi, 1%1$c    syscall%1$c%3$cendmacro%1$cFT_MAIN"
+    string db ";This is a comment in the code%1$c%3$cmacro PRINT_STRING 1%1$c    mov rdi, %3$c1%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, 25%1$c	mov r8, %3$c1%1$c    call printf wrt ..plt%1$c%3$cendmacro%1$c%3$cmacro OPEN_FILE 1%1$c    mov rax, 2%1$c    mov rdi, %3$c1%1$c    mov rsi, 2%1$c    mov rdx, 420%1$c    syscall%1$c%3$cendmacro%1$c%3$cmacro FT_MAIN 0%1$csection .data%1$c    string db %2$c%4$c%2$c%1$c    file_name db %2$cGrace_kid.s%2$c%1$csection .text%1$c    global _start%1$c    extern printf%1$c_start:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    OPEN_FILE file_name%1$c    cmp rax, 0%1$c    jl return_error%1$c    mov rdi, rax%1$c    push rdi%1$c    mov rax, 33%1$c    mov rsi, 1%1$c    syscall%1$c    cmp rax, 0%1$c    jl closefile_and_error%1$c    PRINT_STRING string%1$c    pop rdi%1$c    mov rax, 3%1$c    syscall%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    mov rax, 60%1$c    mov rdi, 0%1$c    syscall%1$cclosefile_and_error:%1$c    pop rdi%1$c    mov rax, 3%1$c    syscall%1$creturn_error:%1$c    add rsp, 16%1$c    mov rsp, rbp%1$c    pop rbp%1$c    mov rax, 60%1$c    mov rdi, 1%1$c    syscall%1$c%3$cendmacro%1$cFT_MAIN%1$c%1$csection .note.GNU-stack%1$c"
     file_name db "Grace_kid.s"
 section .text
     global _start
@@ -58,3 +58,5 @@ return_error:
     syscall
 %endmacro
 FT_MAIN
+
+section .note.GNU-stack
