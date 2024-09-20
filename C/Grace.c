c8ebc8b08734e495bf5e355ad640dc1560e4f8ed
Carlos Orellana <corellana1988@gmail.com>
2024-09-20 16:27:21 +0300

    Grace in C now prints error messages if the file doesn't have permissions to create the file

--- C/Grace.c
+++ C/Grace.c
@@ -7,8 +7,8 @@
  Comment here
 */
 
-#define TOPRINT(str) (printf(str, 10, 34, str))
+#define TOPRINT(str) (printf(str, 10, 34, 92, str))
 #define REDIRECT(fd) (dup2(fd, STDOUT_FILENO))
-#define FT_MAIN()int main(void) { const char *str = "#include <stdio.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$c%1$c/*%1$c Comment here%1$c*/%1$c%1$c#define TOPRINT(str) (printf(str, 10, 34, str))%1$c#define REDIRECT(fd) (dup2(fd, STDOUT_FILENO))%1$c#define FT_MAIN()int main(void) { const char *str = %2$c%3$s%2$c; int fd = open(%2$cGrace_kid.c%2$c, O_RDWR | O_CREAT, 0644); if (fd == -1) { exit(1); } if (REDIRECT(fd) == -1) { close(fd); exit(1); } TOPRINT(str); close(fd); return (0); }%1$c%1$cFT_MAIN()%1$c"; int fd = open("Grace_kid.c", O_RDWR | O_CREAT, 0644); if (fd == -1) { exit(1); } if (REDIRECT(fd) == -1) { close(fd); exit(1); } TOPRINT(str); close(fd); return (0); }
+#define FT_MAIN() int main(void) { const char *str = "#include <stdio.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$c%1$c/*%1$c Comment here%1$c*/%1$c%1$c#define TOPRINT(str) (printf(str, 10, 34, 92, str))%1$c#define REDIRECT(fd) (dup2(fd, STDOUT_FILENO))%1$c#define FT_MAIN() int main(void) { const char *str = %2$c%4$s%2$c; int fd = open(%2$cGrace_kid.c%2$c, O_RDWR | O_CREAT, 0644); if (fd == -1) { write(2, %2$cFailed trying to create/modify Grace_kid.c%3$cn%2$c, 43); exit(1); } if (REDIRECT(fd) == -1) { write(2, %2$cError trying to write the information of the program into Grace_kid.c file%3$cn%2$c, 75); close(fd); exit(1); } close(fd); TOPRINT(str); return (0); }%1$c%1$cFT_MAIN()%1$c"; int fd = open("Grace_kid.c", O_RDWR | O_CREAT, 0644); if (fd == -1) { write(2, "Failed trying to create/modify Grace_kid.c\n", 43); exit(1); } if (REDIRECT(fd) == -1) { write(2, "Error trying to write the information of the program into Grace_kid.c file\n", 75); close(fd); exit(1); } close(fd); TOPRINT(str); return (0); }
 
 FT_MAIN()
